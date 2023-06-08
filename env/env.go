8a4e3ce12054893ce903f146fc7a043717c1331b
xjester0 <xjester0@gmail.com>
2023-06-08 14:10:31 +0300

    rename old methods to String, Int, Duration add new method: Strings

--- env/env.go
+++ env/env.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -15,27 +16,44 @@ func init() {
 		log.Println(err)
 	}
 	dir := filepath.Dir(path)
-
 	_ = godotenv.Load(dir + "/.env") // include local .env file
 }
 
-func Get(key string, defaultValue ...string) string {
+// String return env value as string or default value
+func String(key string, defaultValue ...string) string {
 	return defaultString(os.Getenv(key), defaultValue)
 }
 
-func GetInt(key string, defaultValue ...int) int {
-	value, err := strconv.Atoi(Get(key))
-	if err != nil {
-		if len(defaultValue) > 0 {
-			return defaultValue[0]
-		} else {
-			return 0
-		}
+// Strings return env value as string slice separated by sep or default value
+func Strings(key string, sep string, defaultValue ...[]string) []string {
+	value := make([]string, 0)
+
+	for _, item := range strings.Split(String(os.Getenv(key)), sep) {
+		value = append(value, strings.Trim(item, " "))
 	}
+
+	if len(value) < 1 && len(defaultValue) > 0 {
+		return defaultValue[0]
+	}
+
 	return value
 }
 
-func GetDuration(key string, defaultValue ...string) time.Duration {
+// Int return env value as int or default value
+func Int(key string, defaultValue ...int) int {
+	value, _ := strconv.Atoi(String(key))
+
+	if value == 0 && len(defaultValue) > 0 {
+		return defaultValue[0]
+	}
+
+	return value
+}
+
+// Duration return env value as time.Duration parsed from string format or default value
+// such as "300ms", "-1.5h" or "2h45m"
+// valid time units are "ns", "us" (or "µs"), "ms", "s", "m", "h"
+func Duration(key string, defaultValue ...string) time.Duration {
 	t, _ := time.ParseDuration(defaultString(os.Getenv(key), defaultValue))
 	return t
 }
